a5df557e3a50ed72cdb1bf66587bee620a2dd514
hasnum <hasnum@163.com>
2023-05-15 14:57:56 +0800

    ✨ feat(prettier):

--- packages/hasnum-cli/src/index.js
+++ packages/hasnum-cli/src/index.js
@@ -1,7 +1,7 @@
 console.log('hello world');
 function test() {
   return (
-    <div test='123213' test1='12321312' ttt='erewrewr8' jfdslf=' sdffdsf sd' test='123213' test1='12321312' ttt='erewrewr8'>
+    <div test='dfdsf ' test1='12321312' ttt='erewrewr8' jfdslf=' sdffdsf sd' test='123213' test1='12321312' ttt='erewrewr8'>
       23
     </div>
   );
